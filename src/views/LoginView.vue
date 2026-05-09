6ce9b095ffe6527c02ff934a9fb8e26d2e17f37f
Alan de Jesus Ayala Galvan <alan.ayala@facturatech.co>
2026-05-08 21:23:36 -0600

    chore: configure tailwind v4 custom theme and color palette

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>
-    <h1>Login</h1>
+  <!-- bg-space-dark debería ser el azul oscuro que definimos -->
+  <div class="min-h-screen flex items-center justify-center bg-space-dark">
+    <div class="p-10 bg-white rounded-xl shadow-2xl">
+      <!-- text-portal-green debería ser el verde neón -->
+      <h1 class="text-5xl font-bold text-portal-green">Login</h1>
+      <p class="text-science-cyan mt-2">Testing custom palette...</p>
+    </div>
   </div>
 </template>
