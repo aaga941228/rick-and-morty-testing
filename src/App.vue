9dc7f66b39dfaafc77964edd2ffd4364490884d4
Alan de Jesus Ayala Galvan <alan.ayala@facturatech.co>
2026-05-08 20:53:07 -0600

    chore: setup Tailwind CSS v4, PostCSS, Lucide icons and project structure

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="min-h-screen bg-gray-100 flex items-center justify-center">
+    <h1 class="text-4xl font-bold text-blue-600 drop-shadow-md">¡Tailwind Funcionando! 🚀</h1>
+  </div>
 </template>
 
 <style scoped></style>
