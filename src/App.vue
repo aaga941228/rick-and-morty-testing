11f79224021bbbf732428da2edc35b70d3a554e6
Alan de Jesus Ayala Galvan <alan.ayala@facturatech.co>
2026-05-08 21:10:27 -0600

    feat: setup router paths and create login and dashboard view shells

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="min-h-screen bg-gray-100 flex items-center justify-center">
-    <h1 class="text-4xl font-bold text-blue-600 drop-shadow-md">¡Tailwind Funcionando! 🚀</h1>
-  </div>
+  <router-view></router-view>
 </template>
 
 <style scoped></style>
